6d079f82e6a9c92d54d2cf3730323714a7c9c135
Irenekarlsen <70151255+Irenekarlsen@users.noreply.github.com>
2020-09-13 14:52:21 +0200

    jh

--- A1.py
+++ A1.py
@@ -5,21 +5,23 @@ import matplotlib.pyplot as plt
 
 
 def P(a, b, my, sigma):
-    f = lambda x: (1 /(np.sqrt(2*np.pi)*sigma)) * np.exp(-0.5 *((x-my)/sigma)**2)
-    return integrate.quad(f, a, b)[0]  # Integrerer sannsynlighetstettheten fra a til b
+    f = lambda x:(1 /(np.sqrt(2*np.pi)*sigma)) *np.exp(-0.5 *((x-my)/sigma)**2)
+    # Integrerer sannsynlighetstettheten fra a til b
+    return integrate.quad(f, a, b)[0]
 
-# Printer ut sannsynligheten for at et tiilfeldig tall er innenfor standardavviket sigma
-print(P(-1, 1, 0, 1))
-print(P(-2, 2, 0, 1))
-print(P(-3, 3, 0, 1))
+# Printer ut sannsynligheten for at et tiilfeldig tall er innenfor
+# standardavviket sigma
+print(f'standardavik 1*sigma: {P(-1, 1, 0, 1)}')
+print(f'standardavik 2*sigma: {P(-2, 2, 0, 1)}')
+print(f'standardavik 3*sigma: {P(-3, 3, 0, 1)}')
 
 
 
 #  the Maxwell-Boltzmann Distribution
-N = 10**5
-T = 3000
-k = 1.38 * 10**(-23)                                 # Boltzmann-konstanten
-m = 2*1.66*10**(-27)
+N = 10**5                                    # Anntall partikler
+T = 3000                                     # Temperatur
+k = 1.38 * 10**(-23)                         # Boltzmann-konstanten
+m = 2*1.66*10**(-27)                         # Massen til et hydrogenmolekyl
 
 sigma = np.sqrt((k*T) /m); my = 0
 f = lambda x: (1 /(np.sqrt(2*np.pi)*sigma)) * np.exp(-0.5 *((x-my)/sigma)**2)
@@ -32,8 +34,8 @@ plt.ylabel('Sannsynlighet')
 plt.show()
 
 
-print(P(5, 30, 0, 1) * N)
-# antalltettheteten ^^
+print(f'antalltettheten: {P(5, 30, 0, 1) * N}')
+# Når vi multipliserer med N får vi antalltettheten
 
 
 v = np.linspace(0, 3*10**4, N)
@@ -43,10 +45,3 @@ plt.xlabel('Hastighet'); plt.ylabel('Sannsynlighet')
 plt.show()
 # Dette er ikke i konflikt, da vi kun ser på verdier fra 0 til tre
 # da v kun kan være positiv
-
-def P_(m, k, T, a, b):
-    p = lambda v: v*(m/(2*np.pi*k*T))**(3/2) * np.exp(-(1/2)*(m*v**2/(k*T))) *4*np.pi*v**2
-    return integrate.quad(p, a, b)[0]
-
-k = 1.38 * 10**(-23)
-print(P_(1, k, 3000, 0, np.inf))
