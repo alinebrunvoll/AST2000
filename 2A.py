ff91dc20cd50decf35eb6c76057558d18d7fcf59
Irenekarlsen <70151255+Irenekarlsen@users.noreply.github.com>
2020-09-22 17:10:18 +0200

    we dit it!
    we dit it we dit it!!

--- 2A.py
+++ 2A.py
@@ -1,103 +1,69 @@
 #  EGEN KODE
+
+'''
+Calculating our planet trajectories analytical and numerical
+and actually getting a satisfying awnser!!
+'''
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 import ast2000tools.constants as const
 import ast2000tools.utils as utils
 from ast2000tools.space_mission import SpaceMission
 from ast2000tools.solar_system import SolarSystem
+from numba import njit
 seed = utils.get_seed('alinerb')
 system = SolarSystem(seed)
+mission = SpaceMission(seed)
 
-def simulate_orbits(system, duration, n_time_steps_per_year):
-    G = 4*np.pi**2            # in Astronomical uits (AU)
-    '''
-    Here you can implement the orbital simulation for challenge A
-    of Part 2 of the project.
-    '''
+
+
+def simulate_orbits(system, rot, n_time_steps_per_year):
+    G = 4*np.pi**2                      # in Astronomical uits (AU)
     a = system.semi_major_axes
     e = system.eccentricities
     f = np.linspace(0, 2*np.pi, n_time_steps_per_year)
     for i in range(system.number_of_planets):
-        r_ = (a[i]*(1-e[i]**2)) / (1 + e[i]*np.cos(f))
+        r_ = (a[i]*(1-e[i]**2)) / (1 + e[i]*np.cos(np.pi - system.aphelion_angles[i] + f))
         x = r_*np.cos(f)
         y = r_*np.sin(f)
         plt.plot(x, y)
-    plt.plot(0, 0, 'yo')
-    plt.title('Our solar system')
-    plt.show()
-
+    plt.plot(0, 0, 'yo', label = 'Stjerna')
+    plt.legend()
+    plt.title('Wolfram Alpha')
 
     planet_mass = system.masses
+    P = np.sqrt(4*np.pi**2/(G*(planet_mass[0]+system.star_mass)) *a[0]**3)
     init_x, init_y = system.initial_positions
     init_vel_x, init_vel_y = system.initial_velocities
-    # init_f = system.initial_orbital_angles
-    dt = duration/n_time_steps_per_year
-    r = np.zeros([n_time_steps_per_year, 7, 2])
-    # v_0 = np.zeros([7, 2])
-    v = np.zeros([n_time_steps_per_year, 7, 2])
-    acceleration = np.zeros([n_time_steps_per_year, 7, 2])
-    acceleration_1 = np.zeros([n_time_steps_per_year, 7, 2])
-    times = np.zeros([n_time_steps_per_year])
 
-    for t in range(n_time_steps_per_year):
-        for i in range(system.number_of_planets):
-            v[0, i] = [init_vel_x[i], init_vel_y[i]]
-            r[0, i] = [init_x[i], init_y[i]]
-            acceleration[t, i, :] = -(G*(system.star_mass + planet_mass[i])) /np.linalg.norm(r[t, i, :])**3 * r[t, i, :]
+    r = np.zeros((n_time_steps_per_year*rot, 7, 2))
+    v = np.zeros((n_time_steps_per_year*rot, 7, 2))
+    acceleration = np.zeros((n_time_steps_per_year*rot, 7, 2))
+    times = np.linspace(0, rot*P, rot*n_time_steps_per_year)
+    dt = times[1] - times[0]
 
-            # times += dt
-            # r[t, i, :] += v[t, i, :]*dt + 0.5*acceleration[t, i, :]*dt**2
-            # acceleration_1[t, i, :] = -(G*(system.star_mass + planet_mass[i])) /np.linalg.norm(r[t, i, :])**3 * r[t, i, :]
-            # v[t, i, :] += 0.5*(acceleration[t, i, :] + acceleration_1[t, i, :])*dt
-            # acceleration[t, i, :] = acceleration_1[t, i, :]
+    for i in range(system.number_of_planets):
+        v_0 = np.array([init_vel_x[i], init_vel_y[i]])
+        r_0 = np.array([init_x[i], init_y[i]])
+        acceleration[0, i] = -(G*(system.star_mass) / np.linalg.norm(r_0)**3) * r_0
+        r[0, i] = r_0
+        v[0, i] = v_0
 
-            acceleration[t+1, i, :] = -(G*(system.star_mass + planet_mass[i])) /np.linalg.norm(r[t, i, :])**3 * r[t, i, :]
-            r[t+1, i, :] = r[t, i, :] + v[t, i, :]*dt + 0.5*acceleration[t, i, :]*dt**2
-            v[t+1, i, :] = v[t, i, :] + 0.5*(acceleration[t, i, :] + acceleration[t+1, i, :])*dt
+        for t in range(1, n_time_steps_per_year*rot):
+            acceleration[t, i] = -(G*(system.star_mass)) /np.linalg.norm(r[t-1, i])**3 * r[t-1, i]
+            r[t, i] = r[t-1, i] + v[t-1, i]*dt + 0.5*acceleration[t-1, i]*dt**2
+            acceleration[t, i] = -(G*(system.star_mass)) /np.linalg.norm(r[t, i])**3 * r[t, i]
+            v[t, i] = v[t-1, i] + 0.5*(acceleration[t-1, i] + acceleration[t, i])*dt
 
-    plt.plot(r[:, :, 0], r[:, :, 1], 'ro')
-    plt.title('Våre planetbaner')
+
+    for planets in range(system.number_of_planets):
+        plt.plot(r[:, planets, 0], r[:, planets, 1])
+    plt.axis('equal')
     plt.show()
-    # P = np.sqrt(a**3)
-    # b = a*np.sqrt(1-e**2)
-    # Circ = 2*np.pi * np.sqrt(0.5*(a**2 + b**2))
 
-    # You will probably also need these quantities:
-    # const.G_sol
-    # system.number_of_planets
-    # system.initial_positions
-    # system.initial_velocities
-    # system.star_mass
+    return times, np.transpose(r), P
 
-    return times, r
-simulate_orbits(system,20,1000)
-'''
-# Prevent the following code from executing when calling `import part_2`
-if __name__ == '__main__':
 
-    # Print a message if a newer version of ast2000tools is available
-    utils.check_for_newer_version()
-
-    # Construct SpaceMission instance for my mission
-    seed = utils.get_seed('alinerb')
-    mission = SpaceMission(seed)
-
-    # Extract associated SolarSystem object
-    system = mission.system
-
-    # Simulate orbits
-    times, planet_positions = simulate_orbits(system,
-                                              duration,
-                                              n_time_steps_per_year)
-
-    # Verify simulated orbits and write exact orbits to planet_trajectories.npy
-    mission.verify_planet_positions(duration,
-                                    planet_positions,
-                                    filename='planet_trajectories.npy')
-
-    # Generate orbit video to view in SSView
-    mission.generate_orbit_video(times,
-                                 planet_positions,
-                                 filename='orbit_video.xml')
-'''
+times, r, P = simulate_orbits(system, 25, 10000)
+print(time.process_time())
